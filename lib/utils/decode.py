ef5584cc373eace6bca59bb342a567f202e6131a
Jonathan Thomas Xavier Dean <jon@dev-server.dev-server.local>
2023-11-13 12:33:29 -0500

    Changing up approach in order to use umt for data extraction

--- lib/utils/decode.py
+++
@@ -1,8 +0,0 @@
-import re
-
-def decode_form_data(data):
-    readable_strings = re.findall(b'[ -~]{4,}', data)
-    decoded_strings = [s.decode('utf-8') for s in readable_strings]
-    binary_data = [byte for byte in data if byte not in readable_strings]
-    interpreted_binary_data = [int(byte) for byte in binary_data]
-    return {"readable_strings": decoded_strings, "binary_data": interpreted_binary_data}
